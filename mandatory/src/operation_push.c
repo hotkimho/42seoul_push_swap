ef7497e0c22c8937d19099c178d2be38dc881af9
hokim <rlaghekd777@naver.com>
2022-03-30 20:29:06 +0900

    None

---
+++ mandatory/src/operation_push.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operation_push.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkim2 <hkim2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 18:27:09 by hkim2             #+#    #+#             */
+/*   Updated: 2022/03/30 20:26:03 by hkim2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	pa(t_stack **a, t_stack **b)
+{
+	if (get_stack_size(*b) == 0)
+		return ;
+	push_node(a, (*b)->data);
+	pop_node(b);
+	write(1, "pa\n", 4);
+}
+
+void	pb(t_stack **b, t_stack **a)
+{
+	if (get_stack_size(*a) == 0)
+		return ;
+	push_node(b, (*a)->data);
+	pop_node(a);
+	write(1, "pb\n", 4);
+}
