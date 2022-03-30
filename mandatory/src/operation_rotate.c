570e4641a5443cf11c2c37ca0969b4e8b3fae494
hokim <rlaghekd777@naver.com>
2022-03-30 19:32:42 +0900

    Add. rotate push 함수

---
+++ mandatory/src/operation_rotate.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operation_rotate.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkim2 <hkim2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 19:24:54 by hkim2             #+#    #+#             */
+/*   Updated: 2022/03/30 19:28:37 by hkim2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	ra(t_stack **a)
+{
+	t_stack	*tmp;
+
+	if (*a == NULL)
+		return;
+	tmp = (*a)->next;
+	(*a)->prev->next = *a;
+	(*a)->next = NULL;
+	tmp->prev = *a;
+	*a = tmp;
+}
+
+void	rb(t_stack **b)
+{
+	t_stack	*tmp;
+
+	if (*b == NULL)
+		return;
+	tmp = (*b)->next;
+	(*b)->prev->next = *b;
+	(*b)->next = NULL;
+	tmp->prev = *b;
+	*b = tmp;
+}
+
+void	rr(t_stack **a, t_stack **b)
+{
+	ra(a);
+	rb(b);
+}
