a5ebae75d3efea32bbd07f14bab9e822d317fa5e
hokim <rlaghekd777@naver.com>
2022-03-31 20:17:39 +0900

    Add. get_pivot(중앙값) 함수

---
+++ mandatory/src/pivot.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pivot.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkim2 <hkim2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 20:15:32 by hkim2             #+#    #+#             */
+/*   Updated: 2022/03/31 20:17:18 by hkim2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+int		get_pivot(t_stack *stk, int size)
+{
+	int	arr[size];
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	while (i < size)
+	{
+		arr[i++] = stk->data;
+		stk = stk->next;
+	}
+	i = 0;
+	while (i < size)
+	{
+		count = 0;
+		j = 0;
+		while (j < size)
+			if (arr[i] < arr[j++])
+				count++;
+		if (count == (size / 2))
+			return (arr[i]);
+		i++;
+	}
+	return (arr[size / 2]);
+}
