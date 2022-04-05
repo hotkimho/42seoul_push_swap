fb91c827679ff95804aa0dfb625afc2c013ca474
hotkimho <rlaghekd777@naver.com>
2022-04-05 17:29:02 +0900

    Add. a_optimization 분할

---
+++ mandatory/src/a_optimization_sub.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkim2 <hkim2@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 16:20:05 by hkim2             #+#    #+#             */
+/*   Updated: 2022/04/04 20:42:00 by hkim2            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	five_last_case(t_stack **a, t_stack **b)
+{
+	exec_num_ra(a, 3);
+	exec_num_sa_rra(a, 3);
+	sa(a);
+	a_exec_three_sort(a, b);
+}
+
+int	a_five_sort(t_stack **a, t_stack **b)
+{
+	if ((*a)->data == get_min_data(*a, 5))
+		a_exec_four_sort(a, b);
+	else if ((*a)->next->data == get_min_data(*a, 5))
+	{
+		sa(a);
+		a_exec_four_sort(a, b);
+	}
+	else if ((*a)->next->next->data == get_min_data(*a, 5))
+	{
+		ra(a);
+		exec_num_sa_rra(a, 1);
+		sa(a);
+		a_exec_three_sort(a, b);
+	}
+	else if ((*a)->next->next->next->data == get_min_data(*a, 5))
+	{
+		exec_num_ra(a, 2);
+		exec_num_sa_rra(a, 2);
+		sa(a);
+		a_exec_three_sort(a, b);
+	}
+	else
+		five_last_case(a, b);
+	return (0);
+}
+
+int	a_five_optimize_sort(t_stack **a, t_stack **b)
+{
+	if ((*a)->data == get_min_data(*a, 5))
+		a_exec_four_sort(a, b);
+	else if ((*a)->next->data == get_min_data(*a, 5))
+	{
+		sa(a);
+		a_exec_four_sort(a, b);
+	}
+	else if ((*a)->next->next->data == get_min_data(*a, 5))
+	{
+		exec_num_ra(a, 2);
+		a_exec_four_sort(a, b);
+	}
+	else if ((*a)->next->next->next->data == get_min_data(*a, 5))
+	{
+		rra(a);
+		rra(a);
+		a_exec_four_sort(a, b);
+	}
+	else
+	{
+		rra(a);
+		a_exec_four_sort(a, b);
+	}
+	return (0);
+}
