842a6a42caba8b6b36c3b9061b6e7150f88531c3
hokim <rlaghekd777@naver.com>
2022-04-06 00:03:05 +0900

    Update Norminette 적용

--- mandatory/include/push_swap.h
+++ mandatory/include/push_swap.h
@@ -10,20 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef PUSH_SWAP_H
+
 # define PUSH_SWAP_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
-typedef struct		s_stack
+
+typedef struct s_stack
 {
-	int		data;
-	struct 	s_stack *next;
-	struct	s_stack *prev;
-}					t_stack;
+	int				data;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}						t_stack;
 
 //error
 void	error_msg(char *message);
@@ -68,11 +69,13 @@ void	validate(char *str);
 char	**ft_error(char **s);
 int		find_size(char *s, char c);
 void	ft_strcpy(char *dest, char *src, int start, int end);
-char	**ft_split(char *s, char c);
-void	ft_init(int *start, int *s_size);
+char	**ft_split(char *s, char c, int start, int s_size);
+void	ft_init(int *start, int *s_size, int *i);
 
 //pivot
 int		get_pivot(t_stack *stk, int size);
+void	init_i_count(int *i, int *count);
+int		return_pivot(int *arr);
 
 //check
 int		check_ascending(t_stack *stk, int size);
@@ -89,18 +92,20 @@ int		init_sorting(t_stack *stk, int size, int *a_count, int *b_count);
 //b_util
 void	b_check_two(t_stack **b);
 void	b_check_three(t_stack **a, t_stack **b);
-void	all_push_B_to_A(t_stack **a, t_stack **b, int size);
+void	all_push_b_to_a(t_stack **a, t_stack **b, int size);
+int		three_sort(t_stack **a, t_stack **b);
 
 //a_sort
-void	A_to_B(t_stack **a, t_stack **b, int size);
+void	a_to_b(t_stack **a, t_stack **b, int size);
 int		a_check_sorting(t_stack **a, t_stack **b, int size);
 int		a_check_push(t_stack *stk, int pivot);
-int		A_to_B_loop(t_stack **a, t_stack **b, int *b_count, int pivot);
+int		a_to_b_loop(t_stack **a, t_stack **b, int *b_count, int pivot);
 int		a_more_than_four(t_stack **a, t_stack **b, int size);
 
 //b_sort
-void	B_to_A(t_stack **a, t_stack **b, int size);
-int		b_check_sorting(t_stack **a , t_stack **b, int size);
+void	b_to_a(t_stack **a, t_stack **b, int size);
+int		b_to_a_loop(t_stack **a, t_stack **b, int *a_count, int pivot);
+int		b_check_sorting(t_stack **a, t_stack **b, int size);
 int		b_check_push(t_stack *stk, int pivot);
 int		b_more_than_four(t_stack **a, t_stack **b, int size);
 
@@ -108,25 +113,14 @@ int		b_more_than_four(t_stack **a, t_stack **b, int size);
 void	a_three_optimize_sort(t_stack **a);
 void	a_exec_three_sort(t_stack **a, t_stack **b);
 void	a_exec_four_sort(t_stack **a, t_stack **b);
-int		a_four_sort(t_stack **a, t_stack **b);
-int		a_four_optimize_sort(t_stack **a, t_stack **b);
-
-//a_optimization_sub
-int		a_five_sort(t_stack **a, t_stack **b);
-int		a_five_optimize_sort(t_stack **a, t_stack **b);
-void	a_five_last_case(t_stack **a, t_stack **b);
+void	a_four_optimize_sort(t_stack **a, t_stack **b);
+void	a_five_optimize_sort(t_stack **a, t_stack **b);
 
 //b_optimization
 void	b_three_optimize_sort(t_stack **b);
 void	b_exec_four_sort(t_stack **a, t_stack **b);
 void	b_exec_three_sort(t_stack **a, t_stack **b);
-void	b_four_sort(t_stack **a, t_stack **b);
 void	b_four_optimize_sort(t_stack **a, t_stack **b);
-
-
-//b_optimization_sub
-void	b_five_sort(t_stack **a, t_stack **b);
-void	b_five_last_case(t_stack **a, t_stack **b);
 void	b_five_optimize_sort(t_stack **a, t_stack **b);
 
 //operation_util
@@ -134,6 +128,5 @@ void	exec_num_ra(t_stack **a, int num);
 void	exec_num_sa_rra(t_stack **a, int num);
 void	exec_num_rb(t_stack **b, int num);
 void	exec_num_sb_rrb(t_stack **b, int num);
-//test
-void	print(t_stack *a, t_stack *b);
+
 #endif
